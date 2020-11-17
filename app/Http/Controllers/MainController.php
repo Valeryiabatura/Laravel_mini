99f2c46ba140a907d9fee38f75064c2d08f0fd9d
Valeryiabatura <Valeryiabatura@gmail.com>
2020-11-17 15:10:31 +0300

    first commit

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Phonebook;
+use Illuminate\Http\Request;
+
+
+class MainController extends Controller
+{
+    public function index()
+    {
+        $users = Phonebook::orderBy('name')->simplePaginate(10);
+        return view('home', compact('users'));
+    }
+    public function search(Request $request){
+        $s = $request->s;
+        $users = Phonebook::where('name', 'LIKE', "%{$s}%")->simplePaginate(10);
+        return view('home', compact('users'));
+    }
+}
